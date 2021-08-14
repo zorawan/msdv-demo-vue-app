a5472766ba36c0fccfd12066249ddbca8767a8b7
zorawan <zora0521@gmail.com>
2021-08-14 03:31:34 +0000

    Deploying to gh-pages from @ zorawan/msdv-demo-vue-app@68d710cb9b5d12ef5d091e87298d3e52031b1cc7 🚀

---
+++ Assignment_5/MyData.js
@@ -0,0 +1,46 @@
+  var MyData = [
+  {
+    "name": "Samsung",
+    "value": 29.47
+  },
+  {
+    "name": "Apple",
+    "value": 26.44
+  },
+  {
+    "name": "Huawei",
+    "value": 9.92
+  },
+  {
+    "name": "Xiaomi",
+    "value": 9.8
+  },
+  {
+    "name": "Oppo",
+    "value": 5.02
+  },
+  {
+    "name": "Motorola",
+    "value": 2.25
+  },
+  {
+    "name": "Vivo",
+    "value": 2.14
+  },
+  {
+    "name": "Unknown",
+    "value": 2.1
+  },
+    {
+    "name": "Realme",
+    "value": 1.84
+  },
+      {
+    "name": "LG",
+    "value": 1.77
+  },
+      {
+    "name": "Other",
+    "value": 9.24
+  }
+];
